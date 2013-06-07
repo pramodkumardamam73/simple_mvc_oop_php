50946294214b2cd0e95870f377c1ae04653d7da1
pramodkumardamam73 <pramodkumar.damam73@gmail.com>
2013-06-07 16:04:27 +0530

    Create includes.php

---
+++ config/includes.php
@@ -0,0 +1,7 @@
+<?php
+include('AppConfig.php');
+include(APP_PATH.'model/Database.php');
+include(APP_PATH.'model/Session.php');
+include(APP_PATH.'model/Functions.php');
+include(APP_PATH.'model/Mail.php');
+?>

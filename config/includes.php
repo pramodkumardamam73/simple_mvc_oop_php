ab2fa7c9652d3dd2542b1b25ed6836043960e461
pramodkumardamam73 <pramodkumar.damam73@gmail.com>
2013-06-07 16:12:10 +0530

    Update includes.php

--- config/includes.php
+++ config/includes.php
@@ -3,5 +3,5 @@ include('AppConfig.php');
 include(APP_PATH.'model/Database.php');
 include(APP_PATH.'model/Session.php');
 include(APP_PATH.'model/Functions.php');
-include(APP_PATH.'model/Mail.php');
+//include every model here
 ?>
